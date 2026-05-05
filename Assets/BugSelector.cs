aeafc2eb4434d58c3b1a9b3bbcb48f3d966513c9
mndecormier <marisa.decormier@gmail.com>
2026-05-05 14:15:20 -0400

    Made connection between the buttons and the views

--- Assets/BugSelector.cs
+++ Assets/BugSelector.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+/*using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class BugSelector : MonoBehaviour
@@ -16,5 +16,34 @@ public class BugSelector : MonoBehaviour
     public void LoadMosquito()
     {
         SceneManager.LoadScene("mosquito-view");
+    }
+}*/
+
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class BugSelector : MonoBehaviour
+{
+    [SerializeField] private Button flyButton;
+    [SerializeField] private Button butterflyButton;
+    [SerializeField] private Button mosquitoButton;
+
+    void Awake()
+    {
+        flyButton.onClick.AddListener(() => {
+            Debug.Log("Fly clicked!");
+            SceneManager.LoadScene("fly-view");
+        });
+
+        butterflyButton.onClick.AddListener(() => {
+            Debug.Log("Butterfly clicked!");
+            SceneManager.LoadScene("butterfly-view");
+        });
+
+        mosquitoButton.onClick.AddListener(() => {
+            Debug.Log("Mosquito clicked!");
+            SceneManager.LoadScene("mosquito-view");
+        });
     }
 }
